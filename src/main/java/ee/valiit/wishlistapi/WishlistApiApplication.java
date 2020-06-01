0ad52e07cef2012231bc71c2122e6d14642a6ab9
Siim Tsarents <siim.tsarents@gmail.com>
2020-06-01 13:39:34 +0300

    first commit

---
+++ src/main/java/ee/valiit/wishlistapi/WishlistApiApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.wishlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistApiApplication.class, args);
+	}
+
+}
